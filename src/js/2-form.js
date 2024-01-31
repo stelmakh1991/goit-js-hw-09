c0e86b2bcf0a23a5b1a347b9fe30568d8d981ad7
Volodymyr Stelmakh <volodymyr.kai@gmail.com>
2024-01-31 11:59:45 +0200

    HW-09 is done

---
+++ src/js/2-form.js
@@ -0,0 +1,51 @@
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('textarea');
+
+form.addEventListener('input', e => {
+    const userEmail = form.elements.email.value;
+    const userMessage = form.elements.message.value;
+
+    const data = {
+        email: userEmail,
+        message: userMessage,
+    };
+
+    saveToLS(STORAGE_KEY, data);
+});
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const data = loadFromLS(STORAGE_KEY) || {};
+    console.log(data);
+
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+});
+
+function saveToLS(key = 'empty', value = '') {
+    const jsonData = JSON.stringify(value);
+    localStorage.setItem(key, jsonData);
+}
+
+function loadFromLS(key = 'empty') {
+    const data = localStorage.getItem(key);
+
+    try {
+        const result = JSON.parse(data);
+        return result;
+    } catch {
+        return data;
+    }
+}
+
+function restoreData() {
+    const data = loadFromLS(STORAGE_KEY) || {};
+
+    form.elements.email.value = data.email || '';
+    form.elements.message.value = data.message || '';
+}
+
+restoreData();
